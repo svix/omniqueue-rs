accd46097fcc46843767968569f69d00444b183e
Jonas Platte <jplatte@svix.com>
2024-10-07 10:30:50 +0200

    Revert cfg change
    The redis_sentinel feature activates the redis feature, so this was a
    no-op.

--- omniqueue/tests/it/main.rs
+++ omniqueue/tests/it/main.rs
@@ -4,7 +4,7 @@ mod azure_queue_storage;
 mod gcp_pubsub;
 #[cfg(feature = "rabbitmq")]
 mod rabbitmq;
-#[cfg(any(feature = "redis", feature = "redis_sentinel"))]
+#[cfg(feature = "redis")]
 mod redis;
 #[cfg(feature = "redis_cluster")]
 mod redis_cluster;
